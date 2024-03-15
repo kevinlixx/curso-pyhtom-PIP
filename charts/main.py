80ddfe87b60b247115c08eb2986a4531c544ef7d
kevin Lis <kevin.lis@uniminuto.edu.co>
2024-03-15 18:57:23 -0500

    documentación

---
+++ charts/main.py
@@ -0,0 +1,7 @@
+import charts
+def run():
+    charts.generate_pie_chart()
+
+if __name__ == '__main__': #si el archivo es ejecutado directamente desde la terminal o consola de comandos se ejecutará el código de la función run
+    run()
+
