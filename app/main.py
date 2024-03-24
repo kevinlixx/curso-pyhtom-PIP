f175d23400a43343b998c3d4a2b4731abadb8af8
kevinLis_Laptop <kevin.lis@uniminuto.edu.co>
2024-03-23 20:39:42 -0500

    pandas

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
 import mod
 import read_csv
 import charts
+import pandas as pd
 
 
 def run():
+  '''
+  #manualmente
   data = read_csv.read_csv(
       './data.csv'
   )  #esta linea de codigo lo que hace es llamar a la funcion read_csv que esta en el modulo read_csv
@@ -13,9 +16,21 @@ def run():
   
   porcentages = list(map(lambda x: x['World Population Percentage'], data))#esta linea de codigo lo que hace es crear una lista con los porcentajes de la lista de diccion
   
+  '''
+
+#con pandas 
+  df = pd.read_csv('data.csv')
+  df = df[df['Continent'] == 'Africa']
+
+  countries = df['Country/Territory'].values
+  porcentages = df['World Population Percentage'].values
   charts.generate_pie_chart(countries, porcentages)  #esta linea de codigo lo que hace es generar un grafico de torta con los datos de 
+
+  data = read_csv.read_csv('./data.csv')
   country = input('Type Country => ')
-  
+  print(countries)
+
+
   result = mod.population_by_country(data, country)
   
   if len(result) > 0:
